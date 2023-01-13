a2f21c3441c820b626fbcb3bb266bd22ec3e2a22
Anold Kupara <kuparaanold20@gmail.com>
2023-01-13 14:28:36 +0200

    configured the dbcontext and also created the context class for the project

---
+++ HotelListing.API/Data/HotelListingDbContext.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Data
+{
+    public class HotelListingDbContext : DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
+
+
