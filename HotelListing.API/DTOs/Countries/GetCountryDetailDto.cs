8916c3a54a2f694d5a5331a924cc52ab8c784941
Anold Kupara <kuparaanold20@gmail.com>
2023-01-16 12:03:05 +0200

    created the GetCountryDetailDto model class

---
+++ HotelListing.API/DTOs/Countries/GetCountryDetailDto.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelListing.API.DTOs.Countries
+{
+    public class GetCountryDetailDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+    }
+}
