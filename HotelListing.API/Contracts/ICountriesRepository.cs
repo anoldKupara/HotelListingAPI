05fc8df99aa15e0290164e73a707a5268dee1a95
Anold Kupara <kuparaanold20@gmail.com>
2023-01-17 11:57:44 +0200

    added the ICountriesRepository interface

---
+++ HotelListing.API/Contracts/ICountriesRepository.cs
@@ -0,0 +1,37 @@
+﻿using HotelListing.API.Models;
+
+namespace HotelListing.API.Contracts
+{
+    public class ICountriesRepository : IGenericRepository<Country>
+    {
+        Task<Country> IGenericRepository<Country>.AddAsync(Country entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IGenericRepository<Country>.DeleteAsync(Country entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<bool> IGenericRepository<Country>.Exists(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<List<Country>> IGenericRepository<Country>.GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Country> IGenericRepository<Country>.GetAsync(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IGenericRepository<Country>.UpdateAsync(Country entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
