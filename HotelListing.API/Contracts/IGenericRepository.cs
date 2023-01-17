b77b4ac77a6af2823b66a5d3d989b1330baae455
Anold Kupara <kuparaanold20@gmail.com>
2023-01-17 11:53:24 +0200

    added the various methods in the IGenericRepository interface

--- HotelListing.API/Contracts/IGenericRepository.cs
+++ HotelListing.API/Contracts/IGenericRepository.cs
@@ -1,6 +1,12 @@
 ﻿namespace HotelListing.API.Contracts
 {
-    public class IGenericRepository
+    public interface IGenericRepository<T> where T : class
     {
+        Task<T> GetAsync(int? id);
+        Task<List<T>> GetAllAsync();
+        Task<T> AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task<bool> Exists(int id);
     }
 }
