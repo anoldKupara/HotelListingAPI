aafd5495a68245095fdd876f190fecfedffab877
Anold Kupara <kuparaanold20@gmail.com>
2023-01-17 11:02:31 +0200

    added the BaseCountryDto class

---
+++ HotelListing.API/DTOs/Countries/BaseCountryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace HotelListing.API.DTOs.Countries
+{
+    public abstract class BaseCountryDto
+    {
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+    }
+}
