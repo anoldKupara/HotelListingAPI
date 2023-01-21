b269a1f022d4453a28d6d622cdadf456944fc97c
Anold Kupara <kuparaanold20@gmail.com>
2023-01-21 23:18:21 +0200

    added and implemented the AuthManager class

--- HotelListing.API/Repository/AuthManager.cs
+++ HotelListing.API/Repository/AuthManager.cs
@@ -1,6 +1,41 @@
-﻿namespace HotelListing.API.Repository
+﻿using AutoMapper;
+using HotelListing.API.Configurations;
+using HotelListing.API.Contracts;
+using HotelListing.API.Data;
+using HotelListing.API.DTOs.Users;
+using Microsoft.AspNetCore.Identity;
+
+namespace HotelListing.API.Repository
 {
-    public class AuthManager
+    public class AuthManager : IAuthManager
     {
+        private readonly IMapper _mapper;
+        private readonly UserManager<ApiUser> _userManager;
+        
+        public AuthManager(IMapper mapper, UserManager<ApiUser> userManager)
+        {
+            _mapper = mapper;
+            _userManager = userManager;
+        }
+
+
+        public async Task<IEnumerable<IdentityError>> Register(ApiUserDto userDto)
+        {
+            var user = _mapper.Map<ApiUser>(userDto);
+            user.UserName = userDto.Email;
+
+            var result = await _userManager.CreateAsync(user, userDto.Password);
+
+            if (result.Succeeded)
+            {
+                var addToRoleResult = await _mapper.AddToRoleAsync(user, "User", _userManager);
+                if (!addToRoleResult.Succeeded)
+                {
+                    return addToRoleResult.Errors;
+                }
+            }
+            return result.Errors;
+        }
+
     }
 }
