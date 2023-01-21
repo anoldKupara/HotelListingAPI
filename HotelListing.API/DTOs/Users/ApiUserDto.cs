92a620db5ff517dcba7a228673c8e4e09b8a34e0
Anold Kupara <kuparaanold20@gmail.com>
2023-01-21 22:46:48 +0200

    added the ApiUserDto class

---
+++ HotelListing.API/DTOs/Users/ApiUserDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelListing.API.DTOs.Users
+{
+    public class ApiUserDto
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
