1f6d4a521e497e4d40093ec63ee696bc5cec5f54
Anold Kupara <kuparaanold20@gmail.com>
2023-01-21 23:23:59 +0200

    added the AccountController

---
+++ HotelListing.API/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using HotelListing.API.Contracts;
+using HotelListing.API.DTOs.Users;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelListing.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAuthManager _authManager;
+
+        public AccountController(IAuthManager authManager)
+        {
+            _authManager = authManager;
+        }
+
+        // api/account/register
+        [HttpPost]
+        [Route("register")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Register([FromBody] ApiUserDto userDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _authManager.Register(userDto);
+            if (!result.Any())
+            {
+                return Ok();
+            }
+
+            foreach (var error in result)
+            {
+                ModelState.TryAddModelError(error.Code, error.Description);
+            }
+
+            return BadRequest(ModelState);
+        }
+    }
+}
